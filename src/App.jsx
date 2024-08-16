500ebf8b690072ff5776e802e91d17877d55a699
Thirsanth <thirsanthg@gmail.com>
2024-08-16 12:46:54 +0530

    Added other components

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import EvenNumbers from './components/EvenNumbers';
+import PrimeNumbers from './components/PrimeNumbers';
+import RandNumbers from './components/RandNumber';
+import FiboNumbers from './components/FiboNumbers';
 function App() {
-  const [numbers, setNumbers] = useState(0);
+  // const [numbers, setNumbers] = useState(0);
   // const [windowPrev,setWindowPrev] = useState([]);
 
   const [output,setOutput] = useState([]);
@@ -13,7 +16,10 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/numbers/e" element={<EvenNumbers numbers={numbers} setNumbers={setNumbers} output={output} setOutput={setOutput} />} />
+      <Route path="/numbers/e" element={<EvenNumbers output={output} setOutput={setOutput} />} />
+      <Route path="/numbers/p" element={<PrimeNumbers output={output} setOutput={setOutput} />} />
+      <Route path="/numbers/r" element={<RandNumbers output={output} setOutput={setOutput} />} />
+      <Route path="/numbers/f" element={<FiboNumbers output={output} setOutput={setOutput} />} />
     </Routes>
     </BrowserRouter>
   )
--- src/components/FiboNumbers.jsx
+++ src/components/FiboNumbers.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-export default function EvenNumbers({numbers,setNumbers,output,setOutput}) {
+export default function FiboNumbers({numbers,setNumbers,output,setOutput}) {
 
     // fetch("http://20.244.56.144/test/even",{
     //     method:"GET",
@@ -16,7 +16,7 @@ export default function EvenNumbers({numbers,setNumbers,output,setOutput}) {
 
     useEffect(function(){
         setInterval(()=>{
-            fetch("http://20.244.56.144/test/even",{
+            fetch("http://20.244.56.144/test/fibo",{
                 method:"GET",
                 headers:{
                     "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIzNzkyNTM0LCJpYXQiOjE3MjM3OTIyMzQsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImY4YmFlY2RiLTQwNmQtNDE2NC04YWIyLTM5NzcwNGZkODA2MyIsInN1YiI6IjIxYnRyY3MwMTlAamFpbnVuaXZlcnNpdHkuYWMuaW4ifSwiY29tcGFueU5hbWUiOiJPcmlnaW5tYXJ0IiwiY2xpZW50SUQiOiJmOGJhZWNkYi00MDZkLTQxNjQtOGFiMi0zOTc3MDRmZDgwNjMiLCJjbGllbnRTZWNyZXQiOiJlam9vTGVEcGhBY2Z4cUlLIiwib3duZXJOYW1lIjoiVGhpcnNhbnRoIiwib3duZXJFbWFpbCI6IjIxYnRyY3MwMTlAamFpbnVuaXZlcnNpdHkuYWMuaW4iLCJyb2xsTm8iOiIyMUJUUkNTMDE5In0.DWhjSG8N3WKdMGzTxWULm8zusPKet83CQk20BeSI8wk",
@@ -27,7 +27,7 @@ export default function EvenNumbers({numbers,setNumbers,output,setOutput}) {
                 const json = await d.json();
                 setOutput(json.numbers);
             })
-        },1000)
+        },3000)
     },[output])
   return (
     <div> <h2>WindowPrevState:[{output}]</h2>
